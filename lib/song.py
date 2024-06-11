c08f2ed556d4de586abee483d35b27144decc46b
yassinyussuf <yassinyussuf414@gmail.com>
2024-06-11 08:20:40 +0300

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,18 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+
+        pass    
